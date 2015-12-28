b32b139e35f25c7024127057217c58712beefc87
Pavan Krishnamurthy <pavankrish106@gmail.com>
2015-12-28 18:37:11 +0000

    Search automatically as the string is entered by user

--- app/src/main/java/com/pa/eg/wikimediasearch/MainActivity.java
+++ app/src/main/java/com/pa/eg/wikimediasearch/MainActivity.java
@@ -9,6 +9,9 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,6 +30,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -37,7 +42,8 @@ public class MainActivity extends ActionBarActivity {
     TextView webResponse;
     ArrayList<HashMap<String, String>> pageList;
     GridView lv;
-    String baseUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail&pithumbsize=200&pilimit=100&generator=prefixsearch&gpssearch=";
+    String baseUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail&pithumbsize=100&pilimit=100&generator=prefixsearch&gpssearch=";
+    int activeSearch = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +53,54 @@ public class MainActivity extends ActionBarActivity {
         searchString = (EditText) findViewById(R.id.search_string);
         webResponse = (TextView) findViewById(R.id.responseString);
         lv = (GridView)findViewById(R.id.gridView);
+
+        searchString.addTextChangedListener(new TextWatcher() {
+            Timer timer;
+            long DELAY = 2000;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d("MainAcitivity","beforeTextChanged");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("MainAcitivity","onTextChanged");
+                if (timer != null) {
+                    Log.d("MainAcitivity","onTextChanged timer cancelled");
+                    timer.cancel();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+                final String searchString = s.toString();
+                Log.d("MainAcitivity","afterTextChanged searchString" + searchString);
+
+                if (searchString.length() >= 3) {
+                    timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable () {
+                                @Override
+                                public void run() {
+                                    Log.d("MainActivity", "runnable running activesearch : " +
+                                            activeSearch + "string: " + searchString);
+                                    if (activeSearch == 0 ) {
+                                        Toast msg = Toast.makeText(getApplicationContext(),
+                                                "searching string: " + searchString,
+                                                Toast.LENGTH_LONG);
+                                        msg.show();
+                                        searchWikiImages(searchString);
+                                    }
+                                }
+                            });
+                        }
+
+                    }, DELAY);
+                }
+            }
+        });
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
@@ -68,10 +122,14 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onSearchButtonClicked (View view) {
-        android.util.Log.d("MainActivity","Searching .. ");
-        String searchWord = null;
+        String searchWord = searchString.getText().toString();
+        searchWikiImages(searchWord);
+    }
+
+    public void searchWikiImages(String searchWord) {
+        android.util.Log.d("MainActivity","Searching:  " + searchWord);
         String wikiUrl = null;
-        searchWord = searchString.getText().toString();
+
         pageList = new ArrayList<HashMap<String, String>>();
 
         if(searchWord.matches("")) {
@@ -90,6 +148,7 @@ public class MainActivity extends ActionBarActivity {
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
                 new DownloadWebpage().execute(wikiUrl);
+                activeSearch++;
             } else {
                 webResponse.setText("No network connection available.");
             }
@@ -110,6 +169,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            //searchActive = true;
             progressDialog = new ProgressDialog(MainActivity.this);
             progressDialog.setMessage("Please wait...");
             progressDialog.setCancelable(false);
@@ -120,31 +180,33 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected String doInBackground(String... urls) {
             WebDownloader webDownloader = new WebDownloader();
-            String response;
+            String httpResponse;
+            String result = "Images downloaded";
             try {
-                response = webDownloader.downloadUrl(urls[0]);
+                httpResponse = webDownloader.downloadUrl(urls[0]);
             } catch (IOException e) {
                 return "Unable to retrieve web page. URL may be invalid.";
             }
-            if(response != null) {
-                parseJson(response);
+            if(httpResponse != null) {
+                result = parseJson(httpResponse);
             }
 
-            return response;
+            return result;
         }
 
         @Override
         protected void onPostExecute(String result) {
             if (progressDialog.isShowing())
                 progressDialog.dismiss();
-            webResponse.setText(" Images downloaded ");
+            webResponse.setText(result);
 
             ImageViewAdapter adapter = new ImageViewAdapter(getApplicationContext(),pageList);
             lv.setAdapter(adapter);
+            activeSearch--;
         }
     }
 
-    public void parseJson(String jsonString) {
+    public String parseJson(String jsonString) {
         try {
             JSONObject jsonObj = new JSONObject(jsonString);
 
@@ -172,13 +234,15 @@ public class MainActivity extends ActionBarActivity {
 
                 } else {
                     android.util.Log.d("MainActivity","Image Source: NULL");
-                    pageMap.put("imageSource","file:///android_asset/index.png");
+                    pageMap.put("imageSource","file:///android_asset/noimage.png");
                 }
                 pageList.add(pageMap);
             }
         } catch (JSONException e) {
             e.printStackTrace();
-            //todo end the processing here
+            return "Unable to retrieve web page. Search String may be invalid.";
         }
+
+        return "Wiki pages downloaded";
     }
 }
